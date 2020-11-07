5318758c0fb92cb2ce76cb1044f9c3f465e17895
Emilio <emiliocantu90@hotmail.com>
2020-11-07 06:46:06 -0500

    First Commit

---
+++ Source/UdemyUGD/UdemyUGDGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UdemyUGDGameMode.h"
+#include "UdemyUGDCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUdemyUGDGameMode::AUdemyUGDGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
